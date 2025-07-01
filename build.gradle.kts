f8b278b6c91f910842cbd223b7bbddd3ccef5c26
Kimiega <dmbelitmo@mail.ru>
2025-07-02 02:19:15 +0300

    fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
 }
 
 tasks.jar {
